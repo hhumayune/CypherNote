5b24fdfc79118a4eeced66ef31431e654a0bb1d8
Hamza Humayune <hhumayune@gmail.com>
2023-05-29 20:54:18 +0000

    v4

--- Cypher.js
+++ Cypher.js
@@ -37,8 +37,9 @@ const server = http.createServer((req, res) => {
     });
   } else if (req.url === '/notes' && req.method === 'GET') {
     // Retrieve the encrypted notes from the server
-    const encryptedNotes = notes.map((note) => ({
+    const encryptedNotes = notes.map(note => ({
       encryptedNote: note.encryptedNote,
+      encryptedKey: note.encryptedKey
     }));
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
@@ -51,8 +52,8 @@ const server = http.createServer((req, res) => {
     req.on('end', () => {
       try {
         const data = JSON.parse(body);
-        const { encryptedNote, key } = data;
-        const decryptedNote = decrypt(encryptedNote, Securitykey, initVector, key);
+        const { encryptedNote, encryptedKey, key } = data;
+        const decryptedNote = decrypt(encryptedNote, encryptedKey, key);
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
         res.end(decryptedNote);
@@ -62,55 +63,48 @@ const server = http.createServer((req, res) => {
       }
     });
   } else if (req.url === '/') {
-    // Serve the index.html file
-    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
       if (err) {
         res.statusCode = 500;
         res.end('Internal Server Error');
       } else {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
-        res.end(data);
+        res.end(content);
       }
     });
   } else if (req.url === '/style.css') {
-    // Serve the style.css file
-    fs.readFile(path.join(__dirname, 'style.css'), (err, data) => {
+    fs.readFile(path.join(__dirname, 'style.css'), (err, content) => {
       if (err) {
         res.statusCode = 500;
         res.end('Internal Server Error');
       } else {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/css');
-        res.end(data);
+        res.end(content);
       }
     });
   } else {
     res.statusCode = 404;
-    res.end('Not found');
+    res.end('Not Found');
   }
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-  opn(`http://${hostname}:${port}`);
+  opn(`http://${hostname}:${port}/`);
 });
 
-function encrypt(data, key, iv) {
+function encrypt(text, key, iv) {
   const cipher = crypto.createCipheriv(algorithm, key, iv);
-  let encryptedData = cipher.update(data, 'utf-8', 'hex');
-  encryptedData += cipher.final('hex');
-  return encryptedData;
+  let encrypted = cipher.update(text, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  return encrypted;
 }
 
-function decrypt(encryptedData, key, iv, userKey) {
-  const decryptedKey = decryptData(key, Securitykey, initVector, userKey);
-  return decryptData(encryptedData, decryptedKey, initVector);
-}
-
-function decryptData(data, key, iv) {
-  const decipher = crypto.createDecipheriv(algorithm, key, iv);
-  let decryptedData = decipher.update(data, 'hex', 'utf-8');
-  decryptedData += decipher.final('utf-8');
-  return decryptedData;
+function decrypt(encryptedText, encryptedKey, iv) {
+  const decipher = crypto.createDecipheriv(algorithm, encryptedKey, iv);
+  let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
 }
